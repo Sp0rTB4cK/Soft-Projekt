652e5aa66013747cd031ea6b386f73ea2b11e478
Michael Hahn <m.hahn@fh-bingen.de>
2013-01-15 14:13:48 +0100

    Klasse Mergesort erstellt und Sortierunktionalität implementiert.

---
+++ src/Mergesort.java
@@ -0,0 +1,80 @@
+
+public class Mergesort {
+	
+	private long sortTime;
+	private int[] data;
+	private int[] helpArray;
+
+	public Mergesort(int[] array) {
+		if(array != null) {
+			long start = System.currentTimeMillis();
+			
+			data = array;
+			helpArray = new int[data.length];
+			
+			mergesort(0, data.length - 1);
+			
+			sortTime = System.currentTimeMillis() - start;
+			
+		} else {
+			//Default Werte
+			sortTime = 0;
+			data = new int[] {0};
+		}
+	}
+	
+	public int[] getData() {
+		assert(data != null);
+		return data;
+	}
+	
+	public long getSortTime() {
+		assert(sortTime <= 0);
+		return sortTime;
+	}
+	
+	private void merge(int low, int middle, int high) {
+
+		// Copy both parts into the helper array
+		for (int i = low; i <= high; i++) {
+			helpArray[i] = data[i];
+		}
+
+		int i = low;
+		int j = middle + 1;
+		int k = low;
+		// Copy the smallest values from either the left or the right side back
+		// to the original array
+		while (i <= middle && j <= high) {
+			if (helpArray[i] <= helpArray[j]) {
+				data[k] = helpArray[i];
+				i++;
+			} else {
+				data[k] = helpArray[j];
+				j++;
+			}
+			k++;
+		}
+		// Copy the rest of the left side of the array into the target array
+		while (i <= middle) {
+			data[k] = helpArray[i];
+			k++;
+			i++;
+		}
+	}
+	
+	private void mergesort(int low, int high) {
+	    // Check if low is smaller then high, if not then the array is sorted
+	    if (low < high) {
+	      // Get the index of the element which is in the middle
+	      int middle = low + (high - low) / 2;
+	      // Sort the left side of the array
+	      mergesort(low, middle);
+	      // Sort the right side of the array
+	      mergesort(middle + 1, high);
+	      // Combine them both
+	      merge(low, middle, high);
+	    }
+	  }
+} 
+	  
--- src/Testclass.java
+++ src/Testclass.java
@@ -6,7 +6,7 @@ public class Testclass {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] values = new int[300000];
+		int[] values = new int[3000000];
 		
 		int j = 0;
 		for(int i = values.length-1; i >= 0; i--) {
@@ -18,14 +18,16 @@ public class Testclass {
 			System.out.println(values[i]);
 		}*/
 		
-		Insertionsort is = new Insertionsort(values);
+		//Insertionsort is = new Insertionsort(values);
+		//Mergesort ms = new Mergesort(values);
+		Quicksort qs = new Quicksort(values);
 		
-		System.out.println(is.getSortTime());
+		System.out.println(qs.getSortTime());
 		/*
 		for(int i = 0; i < values.length; i++) {
 			System.out.println(values[i]);
-		}
-		*/
+		}*/
+		
 	}
 
 }
