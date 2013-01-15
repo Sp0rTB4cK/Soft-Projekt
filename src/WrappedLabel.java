ca5c3e98662defbd6a9d5f42410e4c51b14ccbe4
Michael Hahn <m.hahn@fh-bingen.de>
2013-01-15 23:37:37 +0100

    WrappedLabel komplett dokumentiert und Code bereinigt.

--- src/WrappedLabel.java
+++ src/WrappedLabel.java
@@ -1,19 +1,31 @@
 import javax.swing.JLabel;
 import javax.swing.JTextArea;
 
+/**
+ * Die Klasse WrappedLabel dient dazu, Labels mit Zeilenumbruechen zu erzeugen.
+ * 
+ * 
+ * @author michaelhahn
+ *
+ */
 
 public class WrappedLabel extends JTextArea {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	private JLabel label;
 	
+	/**
+	 * Der Standard Konstruktor initialisiert ein WrappedLabel mit einem leeren String.
+	 */
 	public WrappedLabel() {
 		this("");
 	}
-
+	
+	/**
+	 * Dieser Konstruktor initialisiert ein WrappedLabel mit dem uebergebenen String.
+	 * 
+	 * @param text: der Label Text
+	 */
 	public WrappedLabel(String text) {
 		super(text);
 		label = new JLabel();
@@ -24,6 +36,4 @@ public class WrappedLabel extends JTextArea {
         this.setFont(label.getFont());
         this.setOpaque(false);
 	}
-	
-
 }
